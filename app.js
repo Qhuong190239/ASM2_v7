d8ecfbb320174b762c2122194ea99d76959afb0a
Qhuong190239 <huongnqgch190239@fpt.edu.vn>
2021-08-24 00:53:42 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+
+const {getDB, insertStudent, updateStudent, getStudentById, deleteStudent} = require('./databaseHander');
+
+app.set('view engine','hbs')
+app.use(express.urlencoded({extended:true}))
+
+app.get('/edit',async(req, res) => {
+    const id = req.query.id;   
+
+    const s = await getStudentById(id);
+    res.render("edit",{student:s});
+})
+
+app.post('/update', async (req, res) => {
+    const nameInput    = req.body.txtName;
+    const tuoiInput    = req.body.txtTuoi;
+    const id = req.body.txtId;
+
+    updateStudent(id, nameInput, tuoiInput);
+    res.redirect("/")
+})
+
+app.post('/insert', async (req, res) => {
+    const nameInput = req.body.txtName;
+    const tuoiInput = req.body.txtTuoi;
+    const pictureInput = req.body.txtPicture;
+    const newStudent = {name:nameInput, tuoi:tuoiInput, picture:pictureInput}
+
+    insertStudent(newStudent);
+    res.redirect("/")
+})
+app.get('/delete', async (req, res) => {
+    const id = req.query.id;   
+
+    await deleteStudent(id);
+    res.redirect("/");
+})
+
+app.post('/search', async(req, res) => {
+    const searchInput = req.body.txtSearch;
+    const dbo = await getDB();
+    const allStudents = await dbo.collection("students").find({name:searchInput}).toArray();
+
+    res.render('index',{data:allStudents})
+})
+
+app.get('/',async(req, res) => {
+    const dbo = await getDB();
+    const allStudents = await dbo.collection("students").find({}).toArray();
+    res.render('index', {data:allStudents})
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT)
+console.log("app is running", PORT)
+
+
+
---
+++ databaseHander.js
@@ -0,0 +1,50 @@
+const{ObjectId,MongoClient} = require('mongodb');
+const url = 'mongodb://localhost:27017';
+//const url = 'mongodb+srv://Qhuong:251Ha@cluster0.h42lt.mongodb.net/test';
+
+async function getDB() {
+    const client = await MongoClient.connect(url);
+    const dbo = client.db("ASM");
+    return dbo;
+}
+
+async function insertStudent(newStudent) {
+    const dbo = await getDB();
+    await dbo.collection("students").insertOne(newStudent);
+}
+
+async function updateStudent(id, nameInput, tuoiInput) {
+    const filter = { _id: ObjectId(id) };
+    const newValue = { $set: { name: nameInput, tuoi: tuoiInput } };
+
+    const dbo = await getDB();
+    await dbo.collection("students").updateOne(filter, newValue);
+}
+
+async function getStudentById(id) {
+    const dbo = await getDB();
+    const s = await dbo.collection("students").findOne({ _id: ObjectId(id) });
+    return s;
+}
+
+async function deleteStudent(id) {
+    const dbo = await getDB();
+    await dbo.collection("students").deleteOne({ _id: ObjectId(id) });
+}
+
+module.exports = {getDB,insertStudent,updateStudent,getStudentById,deleteStudent}
+
+
+
+
+
+
+
+
+
+
+
+// exports.getDB = getDB;
+// exports.insertStudent = insertStudent;
+// exports.updateStudent = updateStudent;
+// exports.getStudentById = getStudentById;
